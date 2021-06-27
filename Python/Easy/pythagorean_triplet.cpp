c4969ccd227eb002cfd6c1d874827a6ee333f16d
Yash Uppal <officialyashuppal@gmail.com>
2021-06-27 14:44:49 +0530

    27-06-2021 14:44

---
+++ Python/Easy/pythagorean_triplet.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+bool check(int x, int y, int z) {
+    int a = max(x, max(y, z));
+    int b, c;
+    if(a==x){
+        b=y;
+        c=z;
+    }
+    else if(a == y){
+        b= x;
+        c= z;
+    }
+    else {
+        b=x;
+        c=y;
+    }
+    if(a*a == b*b + c*c)
+        return true;
+    else
+        return false;
+    
+}
+
+int main() {
+    int n1,n2,n3;
+    cin>>n1>>n2>>n3;
+    if(check(n1,n2,n3))
+    {
+        cout<<"Pythagorean triplet";
+    }
+    else
+    {
+        cout<<"Not Pythagorean";
+    }
+    return 0;
+    }
